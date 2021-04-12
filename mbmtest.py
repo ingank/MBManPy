839eff811209ca6ec4af836af966a8caea6999fc
Ingolf Ankert <github@ingank.net>
2021-04-12 19:04:43 +0200

    Update mbmtest.py: ADD argparse

--- mbmtest.py
+++ mbmtest.py
@@ -7,28 +7,14 @@
 #
 
 import mbman
-import sys
-import getopt
+import argparse
 
-arg_list = sys.argv[1:]  # erstes Argument der Kommndozeile und darauf folgende
-short_opts = "?hmo:"
-long_opts = ["Help", "My_file", "Output ="]
+parser = argparse.ArgumentParser(
+    description='Ein Programm zur Anwendung des Moduls mbman.py',
+    add_help=False,
+    argument_default='-h'
+)
 
-try:
+parser.add_argument("-h", "--help")
 
-    arguments, values = getopt.getopt(arg_list, short_opts, long_opts)
-
-    for currentArgument, currentValue in arguments:
-
-        if currentArgument in ("-?", "-h", "--Help"):
-            print("Diplaying Help")
-
-        elif currentArgument in ("-m", "--My_file"):
-            print("Displaying file_name:", sys.argv[0])
-
-        elif currentArgument in ("-o", "--Output"):
-            print(("Enabling special output mode (% s)") % (currentValue))
-
-except getopt.error as err:
-    # output error, and return with an error code
-    print(str(err))
+args = parser.parse_args()
