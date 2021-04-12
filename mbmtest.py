2a7363ddaec0be47a86ef96e561799ea7f0ee58f
Ingolf Ankert <github@ingank.net>
2021-04-12 20:17:03 +0200

    Update mbmtest.py: ADD common arguments

--- mbmtest.py
+++ mbmtest.py
@@ -10,11 +10,26 @@ import mbman
 import argparse
 
 parser = argparse.ArgumentParser(
-    description='Ein Programm zur Anwendung des Moduls mbman.py',
-    add_help=False,
-    argument_default='-h'
+    description='Ein Programm zur Anwendung des Moduls mbman.py'
 )
 
-parser.add_argument("-h", "--help")
+parser.add_argument("-v", "--verbose", action="store_true")
+parser.add_argument("-x", "--expunge", action="store_true")
+parser.add_argument("-s", "--server")
+parser.add_argument("-u", "--username")
+parser.add_argument("-p", "--passphrase")
+parser.add_argument("-m", "--mailbox")
+parser.add_argument("-l", "--limit")
+parser.add_argument("--uid")
+parser.add_argument("--connect", action="store_true")
+parser.add_argument("--login", action="store_true")
+parser.add_argument("--get-mb-list", action="store_true")
+parser.add_argument("--get-quota", action="store_true")
+parser.add_argument("--get-info", action="store_true")
+parser.add_argument("--get-message", action="store_true")
+parser.add_argument("--autolimit", action="store_true")
+parser.add_argument("--check-validity", action="store_true")
 
+parser.print_help()
 args = parser.parse_args()
+print(args)
