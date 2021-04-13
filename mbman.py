d75be1206cf901a25c789fa97dfc5f9fa00ce92d
Ingolf Ankert <github@ingank.net>
2021-04-14 01:21:01 +0200

    Update mbman.py: ERR _close in selected state only_ - imaplib.error: command CLOSE illegal in state AUTH, only allowed in states SELECTED

--- mbman.py
+++ mbman.py
@@ -13,13 +13,16 @@ class MBMan:
     def __init__(self, debug):
         self.debug = debug
         self.connected = False
-        self.opened = False
+        self.loggedin = False
+        self.selected = False
         imaplib.Debug = debug
 
     def connect(self, server):
         if (self.connected):
             return
-        if (self.opened):
+        if (self.loggedin):
+            return
+        if (self.selected):
             return
         self.server = server
         self.imap4 = imaplib.IMAP4_SSL(server)
@@ -28,17 +31,18 @@ class MBMan:
     def login(self, user, phrase):
         if (not self.connected):
             return
-        if (self.opened):
+        if (self.loggedin):
+            return
+        if (self.selected):
             return
         self.user = user
         self.phrase = phrase
         self.imap4.login(user, phrase)
-        self.opened = True
 
     def disconnect(self):
-        if (self.opened):
+        if (self.selected):
             self.imap4.close()
-            self.opened = False
+            self.selected = False
         if (self.connected):
             self.imap4.logout()
             self.connected = False
