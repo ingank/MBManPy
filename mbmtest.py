fcd0222b293a25671907ad06c205d9501aec2888
Ingolf Ankert <github@ingank.net>
2021-04-17 01:24:23 +0200

    Update mbmtest.py: MOVE list malbox command

--- mbmtest.py
+++ mbmtest.py
@@ -44,8 +44,8 @@ def parseArgs():
     # Befehle
     parser.add_argument("-C", "--connect", action="store_true", help="connect to imap server")
     parser.add_argument("-L", "--login", action="store_true", help="login to an imap user account")
-    parser.add_argument("-B", "--get-boxes", action="store_true", help="print a list of available mailboxes")
     parser.add_argument("-Q", "--get-quota", action="store_true", help="print infos about quota and usage")
+    parser.add_argument("-B", "--get-boxes", action="store_true", help="print a list of available mailboxes")
     parser.add_argument("-I", "--get-info", action="store_true", help="print some infos about the mbman object")
     parser.add_argument("-M", "--get-message", action="store_true", help="get a message from server")
     parser.add_argument("-A", "--autolimit", action="store_true", help="automatic limit an backup the mailbox")
