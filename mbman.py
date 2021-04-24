de60a5243f7b17eed12ae1c733c37b762780dac8
ingank <github@ingank.net>
2021-04-24 12:57:40 +0200

    Update mbman.py: FIX formatting

--- mbman.py
+++ mbman.py
@@ -94,7 +94,7 @@ class MBMan:
                     if (line[0].find(su) != -1):
                         special = su
                         break
-                folders.append([special,line[1]])
+                folders.append([special, line[1]])
             return folders
         return
 
